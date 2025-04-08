d476ee2bc27bea572d31bcc9984cfdb8cf7ff2a9
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-04-07 23:28:26 -0300

    Primeiro commit (Preparando setup do projeto e styled components)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="success"></Button>
+      <Button variant="danger"></Button>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
