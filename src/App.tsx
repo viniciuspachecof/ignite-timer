0bde9002d277e5d3f738df1bead378ab8c0fbf95
Vinicius Pacheco <vinicius.pachecoof@hotmail.com>
2025-04-08 22:49:08 -0300

    Rotas e estilização componente home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from 'styled-components';
-import { Button } from './components/Button';
+import { BrowserRouter } from 'react-router-dom';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="success"></Button>
-      <Button variant="danger"></Button>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
